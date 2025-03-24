ffaf63edf5fa1be03cbe718ddbe1b6c3102ab727
Caaasperr <9183612@gmail.com>
2025-03-24 17:26:28 +0900

    - 우승자 판별 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,25 @@
 package racingcar;
 
+import java.util.ArrayList;
+
 public class Application {
+
+    public ArrayList<Car> checkWinner(ArrayList<Car> cars) {
+        int highPoint = 0;
+
+        ArrayList<Car> winners = new ArrayList<Car>();
+
+        for (Car car : cars) {
+            if (car.point >= highPoint) { highPoint = car.point; }
+        }
+
+        for (Car car : cars) {
+            if (car.point == highPoint) { winners.add(car); }
+        }
+        return winners;
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        ArrayList<Car> cars = new ArrayList<Car>();
     }
 }
